fd7176a23e7d680c22b667829f40c273adc5343a
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-11-24 14:10:25 -0300

    w2_asign_in_process

--- scripts/temple.js
+++ scripts/temple.js
@@ -1,5 +1,17 @@
+//Año y Fecha en el Footer.
+
 const today = new Date();
 const year = today.getFullYear();
 document.querySelector("#currentyear").textContent = year;
 let lastModification = new Date(document.lastModified)
 document.getElementById("lastModified").textContent = lastModification.toLocaleDateString("en-US");
+
+//hamburguer button
+
+const burgerButton = document.querySelector('#hamburger');
+const navigation = document.querySelector('.navigation');
+
+burgerButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    burgerButton.classList.toggle('open');
+});
