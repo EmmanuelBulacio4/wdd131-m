314c65036194d647f0179b9c06f05d26d7c08fbc
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-11-24 10:24:26 -0300

    w2_Temple_htmlready. footer_js_modf

---
+++ scripts/temple.js
@@ -0,0 +1,5 @@
+const today = new Date();
+const year = today.getFullYear();
+document.querySelector("#currentyear").textContent = year;
+let lastModification = new Date(document.lastModified)
+document.getElementById("lastModified").textContent = lastModification.toLocaleDateString("en-US");
