afb4aee0c57267b0a582da8e9cba3976b0500ba6
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2024-11-29 18:39:39 -0300

    place-page

---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+//Año y Fecha en el Footer.
+
+const today = new Date();
+const year = today.getFullYear();
+document.querySelector("#currentyear").textContent = year;
+let lastModification = new Date(document.lastModified)
+document.getElementById("lastModified").textContent = lastModification.toLocaleDateString("en-US");
